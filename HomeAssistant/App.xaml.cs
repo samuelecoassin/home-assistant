75a486d486e5082b3e96afb909caa4b1e56d9f7f
Samuele Coassin <58360109+samuelecoassin@users.noreply.github.com>
2021-09-23 23:01:37 +0200

    Primo commit

---
+++ HomeAssistant/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HomeAssistant
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
