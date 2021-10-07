e6f0874bc7eacc19a8209ed69dc7f6048a504c96
Samuele Coassin <samuele.coassin@viasatonline.it>
2021-10-07 14:47:24 +0200

    Changes

---
+++ Common/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace HomeAssistant.Common
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
